1dd16a4e32aacb056191b68473194beeeb5da010
WiitterSimithYU <378961614@qq.com>
2017-12-09 13:41:57 +0800

    first update

---
+++ iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/miaomiaokeji/Desktop/TTNews-master/iOSInjectionProject/BundleContents.m \
+  /tmp/injectionforxcode/BundleInjection.h \
+  /tmp/injectionforxcode/BundleInterface.h \
+  /Users/miaomiaokeji/Desktop/TTNews-master/iOSInjectionProject/BundleContents.h
